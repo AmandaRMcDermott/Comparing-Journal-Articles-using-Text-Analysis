10ac3df8b2b0d7fa5c5906266c13a6ac69cea8f3
Amanda McDermott <42814674+Glacieus@users.noreply.github.com>
2018-11-18 19:40:01 -0500

    Upload

--- R Files/Cleaning Article Text.R
+++ R Files/Cleaning Article Text.R
@@ -3,10 +3,10 @@ library(tidytext)
 library(tm)
 library(stringr)
 
-articles_word <- articles %>% 
+articles_word <- article_texts %>% 
   unnest_tokens(word, text)
 
-articles_sent <- articles %>% 
+articles_sent <- article_texts %>% 
   unnest_tokens(sentence, text, token = "sentences")
 
 tidy_words <- articles_word %>% 
@@ -17,7 +17,25 @@ tidy_words %>%
 count(word, sort = T)
 
 
-test <- c("hello \"35\"", "\"524\"Another test" )
+test <- c("hello \"35\"", "\"524\"Another test","hello \",\" \"hey\"")
 writeLines(test)
-clean_articles <- article_texts %>% 
-  transform(text = gsub("\"[0-9]+\"", "", .))
+test <- text %>% 
+  mutate(text = gsub("\"[0-9]+\"", "", text))
+  mutate(text = gsub("\",[a-z]\"", "", .))
+
+str_replace_all(text, "\"[0-9]+\"", "")
+
+
+article_texts %>% 
+  str_detect(text, "\"[0-9]+\"")
+article_texts$text[str_detect(article_texts$text, "\"[0-9]+\"")]
+
+peak <- article_texts %>% 
+  filter(str_detect(article_texts$text, "\"[0-9]+\",\"")) %>% 
+  mutate(text = gsub())
+  
+  
+another_peak <- article_texts %>% 
+  filter(!str_detect(article_texts$text, "\"[0-9]+\",\""))
+
+writeLines("\"[0-9]+\",\"")
